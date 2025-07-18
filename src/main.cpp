73b64ae1439a2fd8dddeaea7738c11e84e8e351f
Vulji <valentin.luzolo@protonmail.com>
2025-07-18 05:46:46 +0200

    Disk analytic

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,17 @@ struct Particle {
 };
 
 
-void spawnParticleInDiskRejection(std::vector<Particle>& particles,
-                                  const glm::vec2& center,
-                                  float R)
+void spawnParticleInDiskAnalytic(std::vector<Particle>& particles,
+                                 const glm::vec2& center,
+                                 float R)
 {
-    glm::vec2 pos;
+    float theta = utils::rand(0.f, 2.f * glm::pi<float>());
 
-    do {
-        float x = utils::rand(-R, R);
-        float y = utils::rand(-R, R);
-        pos = center + glm::vec2(x, y);
-    } while (glm::dot(pos - center, pos - center) > R*R);
+    float u = utils::rand(0.f, 1.f);
+    float r = R * std::sqrt(u);
+
+    glm::vec2 pos = center + glm::vec2(std::cos(theta),
+                                       std::sin(theta)) * r;
 
     Particle p;
     p.position  = pos;
@@ -33,7 +33,6 @@ void spawnParticleInDiskRejection(std::vector<Particle>& particles,
     p.mass      = utils::rand(0.5f, 2.0f);
     p.age       = 0.f;
     p.life_time = utils::rand(2.f, 5.f);
-
     particles.emplace_back(p);
 }
 
@@ -45,32 +44,32 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
     
-    glm::vec2 center = glm::vec2(0.f, 0.f);
+    glm::vec2 center = {0.f, 0.f};
     float     R      = 0.8f;
-
+    
     const int TOTAL_PARTICLES = 1500;
     std::vector<Particle> particles;
     particles.reserve(TOTAL_PARTICLES);
 
     for (int i = 0; i < TOTAL_PARTICLES; ++i)
-        spawnParticleInDiskRejection(particles, center, R);
+        spawnParticleInDiskAnalytic(particles, center, R);
 
     
     while (gl::window_is_open())
     {
         float dt = gl::delta_time_in_seconds();
         glClear(GL_COLOR_BUFFER_BIT);
-
+        
         const float beatFreq   = 1.5f;
         const float beatAmp    = 0.3f;
         const float baseRadius = 0.02f;
-
         for (auto& p : particles)
         {
             p.elapsed += dt;
-            float scale = 1.f + beatAmp * std::sin(2.f * glm::pi<float>() * beatFreq * p.elapsed);
-            float r     = baseRadius * scale;
-            utils::draw_disk(p.position, r, {1,1,1,1});
+            float scale = 1.f + beatAmp
+                * std::sin(2.f * glm::pi<float>() * beatFreq * p.elapsed);
+            float rad = baseRadius * scale;
+            utils::draw_disk(p.position, rad, {1,1,1,1});
         }
     }
 
