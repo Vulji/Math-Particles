115970466c1f123a9f969c3dccae0c4db4f07233
Vulji <valentin.luzolo@protonmail.com>
2025-07-18 05:28:48 +0200

    parallelogram

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,32 @@
 
 struct Particle {
     glm::vec2 position;
-    glm::vec2 velocity;
     float     mass;
+    float elapsed;
     float     age;
     float     life_time;
 };
 
 
-void spawnParticleInRectangle(std::vector<Particle>& particles,
-                              const glm::vec2& min, const glm::vec2& max)
+void spawnParticleInParallelogram(std::vector<Particle>& particles,
+                                  const glm::vec2& offset,
+                                  const glm::vec2& v1,
+                                  const glm::vec2& v2)
 {
+    float u = utils::rand(0.f, 1.f);
+    float v = utils::rand(0.f, 1.f);
+
+    glm::vec2 pos = offset + u * v1 + v * v2;
+
     Particle p;
-    p.position = { utils::rand(min.x, max.x),
-                   utils::rand(min.y, max.y) };
-    float ang   = utils::rand(0.f, 2.f * glm::pi<float>());
-    float spd   = utils::rand(0.2f, 1.0f);
-    p.velocity  = glm::vec2(std::cos(ang), std::sin(ang)) * spd;
-    p.mass      = utils::rand(0.5f, 2.0f);
-    p.age       = 0.f;
-    p.life_time = utils::rand(2.f, 5.f);
+    p.position  = pos;
+    float ang    = utils::rand(0.f, 2.f * glm::pi<float>());
+    float spd    = utils::rand(0.2f, 1.0f);
+    p.elapsed   = 0.f + utils::rand(0.f, 1.f);
+    p.mass       = utils::rand(0.5f, 2.0f);
+    p.age        = 0.f;
+    p.life_time  = utils::rand(2.f, 5.f);
+
     particles.emplace_back(p);
 }
 
@@ -38,57 +45,52 @@ int main()
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
 
-    const glm::vec2 rectMin      = {-0.5f, -0.5f};
-    const glm::vec2 rectMax      = { 0.5f,  0.5f};
-    const glm::vec2 circleCenter = { 0.f,    0.f};
-    const float     circleRadius = 0.4f;
-    const float     particleR    = 0.02f;
-    const glm::vec4 startColor   = {1.f, 1.f, 1.f, 1.f};
-    const glm::vec4 rectColor    = {1.f, 1.f, 0.f, 0.5f};
+    glm::vec2 offset = {-0.5f, -0.5f};
+    glm::vec2 side1  = { 1.0f,  0.0f};
+    glm::vec2 side2  = { 0.5f,  0.8f};
+
+
+    const int TOTAL_PARTICLES = 1500;
 
     std::vector<Particle> particles;
-    particles.reserve(500);
+    particles.reserve(TOTAL_PARTICLES);
 
 
-    for (int i = 0; i < 100; ++i)
-        spawnParticleInRectangle(particles, rectMin, rectMax);
+    for (int i = 0; i < TOTAL_PARTICLES; ++i)
+        spawnParticleInParallelogram(particles, offset, side1, side2);
+
 
     while (gl::window_is_open())
     {
-        float dt = gl::delta_time_in_seconds();
-        glClearColor(0.f, 0.f, 0.f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);
+    float dt = gl::delta_time_in_seconds();
 
-
+    glClear(GL_COLOR_BUFFER_BIT);
 
 
         float thickness = 0.005f;
-        glm::vec2 bl = { rectMin.x, rectMin.y };
-        glm::vec2 br = { rectMax.x, rectMin.y };
-        glm::vec2 tr = { rectMax.x, rectMax.y };
-        glm::vec2 tl = { rectMin.x, rectMax.y };
-        utils::draw_line(bl, br, thickness, rectColor);
-        utils::draw_line(br, tr, thickness, rectColor);
-        utils::draw_line(tr, tl, thickness, rectColor);
-        utils::draw_line(tl, bl, thickness, rectColor);
+        glm::vec2 p0 = offset;
+        glm::vec2 p1 = offset + side1;
+        glm::vec2 p2 = offset + side1 + side2;
+        glm::vec2 p3 = offset + side2;
+        glm::vec4 rectColor = {1,1,0,0.5f};
+        utils::draw_line(p0,p1,thickness,rectColor);
+        utils::draw_line(p1,p2,thickness,rectColor);
+        utils::draw_line(p2,p3,thickness,rectColor);
+        utils::draw_line(p3,p0,thickness,rectColor);
 
-        spawnParticleInRectangle(particles, rectMin, rectMax);
+        const float beatFreq   = 1.5f;
+        const float beatAmp    = 0.3f;
+        const float baseRadius = 0.02f;
 
         for (auto& p : particles)
         {
-            glm::vec2 oldPos = p.position;
-            glm::vec2 newPos = oldPos + p.velocity * dt;
-            glm::vec2 hit;
-
-            float ar = gl::window_aspect_ratio();
-            if (p.position.x >  ar) p.position.x = -ar;
-            if (p.position.x < -ar) p.position.x =  ar;
-            if (p.position.y >  1.f) p.position.y = -1.f;
-            if (p.position.y < -1.f) p.position.y =  1.f;
-
-            utils::draw_disk(p.position, particleR, startColor);
+            p.elapsed += dt;
+            float scale = 1.0f + beatAmp * std::sin(2.f * glm::pi<float>() * beatFreq * p.elapsed);
+            float r     = baseRadius * scale;
+            utils::draw_disk(p.position, r, {1,1,1,1});
         }
     }
 
     return 0;
 }
+
