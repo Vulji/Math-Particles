fc9a07f858fb60c8cb895228ca32f187f551a9a4
Vulji <valentin.luzolo@protonmail.com>
2025-07-18 11:09:33 +0200

    Bézier De Casteljau

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include "poisson_disc.hpp"
+#include <vector>
 
 
 struct Particle {
@@ -37,15 +38,42 @@ void spawnParticleInDiskAnalytic(std::vector<Particle>& particles,
     particles.emplace_back(p);
 }
 
-void draw_parametric(std::function<glm::vec2(float)> const& p,
-                     int segments = 128,
-                     float thickness = 0.003f,
-                     const glm::vec4& color = {1,1,1,1})
+glm::vec2 deCasteljau(std::vector<glm::vec2> points, float t)
 {
+    int n = int(points.size());
+    for (int r = 1; r < n; ++r)
+        for (int i = 0; i < n - r; ++i)
+            points[i] = (1.f - t) * points[i] + t * points[i+1];
+    return points[0];
+}
 
+inline glm::vec2 bezier1(const glm::vec2& p0, const glm::vec2& p1, float t)
+{
+    return (1.f - t) * p0 + t * p1;
+}
+
+inline glm::vec2 bezier2(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2, float t)
+{
+    glm::vec2 p01 = (1.f - t)*p0 + t*p1;
+    glm::vec2 p12 = (1.f - t)*p1 + t*p2;
+    return (1.f - t)*p01 + t*p12;
+}
+
+inline glm::vec2 bezier3(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& p3, float t)
+{
+    glm::vec2 p01 = (1.f - t)*p0 + t*p1;
+    glm::vec2 p12 = (1.f - t)*p1 + t*p2;
+    glm::vec2 p23 = (1.f - t)*p2 + t*p3;
+    glm::vec2 q0  = (1.f - t)*p01 + t*p12;
+    glm::vec2 q1  = (1.f - t)*p12 + t*p23;
+    return (1.f - t)*q0 + t*q1;
+}
+
+void draw_parametric(std::function<glm::vec2(float)> const& p, int segments = 128, float thickness = 0.003f, glm::vec4 color = {1,1,1,1})
+{
     glm::vec2 prev = p(0.f);
-
-    for (int i = 1; i <= segments; ++i) {
+    for (int i = 1; i <= segments; ++i)
+    {
         float t    = float(i) / float(segments);
         glm::vec2 curr = p(t);
         utils::draw_line(prev, curr, thickness, color);
@@ -53,39 +81,82 @@ void draw_parametric(std::function<glm::vec2(float)> const& p,
     }
 }
 
+void draw_bezier1(const glm::vec2& p0, const glm::vec2& p1, int segments = 64, glm::vec4 color = {1,0,0,1})
+{
+    draw_parametric([&](float t){ return bezier1(p0,p1,t); }, segments, 0.004f, color);
+}
+
+void draw_bezier2(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2, int segments = 128, glm::vec4 color = {0,1,0,1})
+{
+    draw_parametric([&](float t){ return bezier2(p0,p1,p2,t); }, segments, 0.004f, color);
+}
+
+void draw_bezier3(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& p3, int segments = 256, glm::vec4 color = {0,0,1,1})
+{
+    draw_parametric([&](float t){ return bezier3(p0,p1,p2,p3,t); }, segments, 0.004f, color);
+}
+
 int main()
 {
-    gl::init("Parametric Curve");
+    gl::init("Interactive Bézier ");
     gl::maximize_window();
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-    glm::vec2 center = {0.f, 0.f};
-    float     scale  = 0.5f;   
+    std::vector<glm::vec2> curve1 = {{-0.8f, -0.5f}, {-0.3f,  0.2f}};
+    std::vector<glm::vec2> curve2 = {{ 0.1f, -0.4f}, { 0.4f,  0.6f}, { 0.7f, -0.1f}};
+    std::vector<glm::vec2> curve3 = {{-0.6f,  0.7f}, {-0.2f, -0.2f}, { 0.3f,  0.8f}, {0.8f, 0.3f}};
 
+    int grabbedCurve = -1;
+    int grabbedIndex = -1;
+    const float pickRadius = 0.03f;
 
     while (gl::window_is_open())
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-       draw_parametric(
-            [&](float t) -> glm::vec2 {
-                
-                float θ = t * 2.f * glm::pi<float>();
-                
-                float x = std::pow(std::sin(θ), 3.f);
-                
-                float y = (13.f*std::cos(θ)
-                         - 5.f*std::cos(2.f*θ)
-                         - 2.f*std::cos(3.f*θ)
-                         -     std::cos(4.f*θ)
-                        ) / 16.f;
-                return center + scale * glm::vec2(x, y);
-            },
-            512,
-            0.003f,
-            {1,1,1,1}
-        );
+        glm::vec2 m = gl::mouse_position();
+
+        if (grabbedCurve < 0)
+        {
+            auto tryHover = [&](auto& curve, int cid) {
+                for (int i = 0; i < (int)curve.size(); ++i)
+                {
+                    if (glm::length(curve[i] - m) < pickRadius)
+                    {
+                        grabbedCurve = cid;
+                        grabbedIndex = i;
+                        return true;
+                    }
+                }
+                return false;
+            };
+            if      (tryHover(curve1, 1)) {}
+            else if (tryHover(curve2, 2)) {}
+            else if (tryHover(curve3, 3)) {}
+        }
+        else
+        {
+            auto& curve = (grabbedCurve == 1 ? curve1
+                          : grabbedCurve == 2 ? curve2
+                                              : curve3);
+            if (glm::length(curve[grabbedIndex] - m) < pickRadius)
+            {
+                curve[grabbedIndex] = m;
+            }
+            else
+            {
+                grabbedCurve = grabbedIndex = -1;
+            }
+        }
+
+        draw_bezier1(curve1[0], curve1[1], 64,   {1,0,0,1});
+        draw_bezier2(curve2[0], curve2[1], curve2[2], 128, {0,1,0,1});
+        draw_bezier3(curve3[0], curve3[1], curve3[2], curve3[3], 256, {0,0,1,1});
+
+        for (auto& p : curve1) utils::draw_disk(p, pickRadius*0.7f, {1,0,0,1});
+        for (auto& p : curve2) utils::draw_disk(p, pickRadius*0.7f, {0,1,0,1});
+        for (auto& p : curve3) utils::draw_disk(p, pickRadius*0.7f, {0,0,1,1});
     }
 
     return 0;
