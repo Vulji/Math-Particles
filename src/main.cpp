9e308bd489c1380c7d491d8c4d7f10411ae07121
Vulji <valentin.luzolo@protonmail.com>
2025-07-18 10:35:52 +0200

    Circle draw

--- src/main.cpp
+++ src/main.cpp
@@ -37,63 +37,46 @@ void spawnParticleInDiskAnalytic(std::vector<Particle>& particles,
     particles.emplace_back(p);
 }
 
+void draw_parametric(std::function<glm::vec2(float)> const& p,
+                     int segments = 128,
+                     float thickness = 0.003f,
+                     const glm::vec4& color = {1,1,1,1})
+{
+
+    glm::vec2 prev = p(0.f);
+
+    for (int i = 1; i <= segments; ++i) {
+        float t    = float(i) / float(segments);
+        glm::vec2 curr = p(t);
+        utils::draw_line(prev, curr, thickness, color);
+        prev = curr;
+    }
+}
 
 int main()
 {
-    gl::init("Particles!");
+    gl::init("Parametric Curve");
     gl::maximize_window();
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
-    
-    glm::vec2 regionSize = {1.f, 1.f};
-    float     radius     = 0.05f;    
-    int       tries      = 30;       
-    
-    auto samplePoints = PoissonDisc::GeneratePoints(radius, regionSize, tries);
 
-
-    std::vector<Particle> particles;
-    particles.reserve(samplePoints.size());
-    for (auto& sp : samplePoints) {
-        Particle p;
-        p.position  = sp;                              
-        p.elapsed   = utils::rand(0.f, 1.f);           
-        p.mass      = utils::rand(0.5f, 2.0f);
-        p.age       = 0.f;
-        p.life_time = utils::rand(2.f, 5.f);
-        particles.emplace_back(p);
-    }
-
-    
-    glm::vec2 offset = {-0.5f, -0.5f};
-
-    
-    const float beatFreq   = 1.5f;   
-    const float beatAmp    = 0.3f;   
-    const float baseRadius = 0.02f;  
+    glm::vec2 center = {0.f, 0.f};
+    float     radius = 0.5f;
 
     while (gl::window_is_open())
     {
-        float dt = gl::delta_time_in_seconds();
         glClear(GL_COLOR_BUFFER_BIT);
 
-        
-        utils::draw_line(offset + glm::vec2(0,0), offset + glm::vec2(1,0), 0.003f, {1,1,0,0.5f});
-        utils::draw_line(offset + glm::vec2(1,0), offset + glm::vec2(1,1), 0.003f, {1,1,0,0.5f});
-        utils::draw_line(offset + glm::vec2(1,1), offset + glm::vec2(0,1), 0.003f, {1,1,0,0.5f});
-        utils::draw_line(offset + glm::vec2(0,1), offset + glm::vec2(0,0), 0.003f, {1,1,0,0.5f});
-
-        
-        for (auto& p : particles)
-        {
-            p.elapsed += dt;
-            float scale = 1.f + beatAmp
-                * std::sin(2.f * glm::pi<float>() * beatFreq * p.elapsed);
-            float r = baseRadius * scale;
-
-            glm::vec2 posNDC = offset + p.position;
-            utils::draw_disk(posNDC, r, {1,1,1,1});
-        }
+        draw_parametric(
+            [&](float t) -> glm::vec2 {
+                
+                float angle = t * 2.f * glm::pi<float>();
+                return center + glm::vec2(std::cos(angle), std::sin(angle)) * radius;
+            },
+            128,
+            0.004f,
+            {1,1,0,1}
+        );
     }
 
     return 0;
